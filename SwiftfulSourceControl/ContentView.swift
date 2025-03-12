2c878b17d8b8092cbec9f0d3f8bc78e8eb28f24f
Morumbi <zun0323@gmail.com>
2025-03-12 14:39:58 -0300

    staging test

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "heart.fill")
-                .font(.title2)
+                .font(.title)
                 .foregroundStyle(.tint)
             Text("Swiftful Tinking!")
 
