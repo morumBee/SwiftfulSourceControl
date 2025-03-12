2929106eaf0b55f8ba0468f298bf50ca31e4a121
MorumBee <zun0323@gmail.com>
2025-03-11 21:03:32 -0300

    Add click me button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful Tinking!")
-            
+
+            Button {}
+                label: {
+                    Text("Click me!")
+                }
         }
         .padding()
     }
