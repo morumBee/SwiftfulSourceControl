2dc78b6450a1772baf8c54f15544a6fda1bd1dfd
MorumBee <zun0323@gmail.com>
2025-03-11 21:06:27 -0300

    Add HomeView

---
+++ HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Morumbi on 3/11/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
