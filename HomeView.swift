765df5082be8e6b2c9daaa90eaa4cdb5ccfb8bd2
MorumBee <zun0323@gmail.com>
2025-03-11 21:08:01 -0300

    HomeView updates (progress)

--- HomeView.swift
+++ HomeView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Screen 2!")
+        ZStack { 
+            Text("Screen 2!")
+            Text("Screen 2!")
+            Text("Screen 2!")
+        }
     }
 }
 
